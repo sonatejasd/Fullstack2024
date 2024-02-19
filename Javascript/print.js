3510aaba111b6c57623dc91db45c4efb6b74fc02
sonatejasd <sonatejasd@gmail.com>
2024-02-19 14:08:12 +0530

    new trials

---
+++ Javascript/print.js
@@ -0,0 +1,74 @@
+// console.log("hello from ext");
+// var a = Number(prompt("enter a number"));
+// var b = Number(prompt("enter an number"));
+// // alert("addition is",a+b);
+// // alert("multiplication is",a*b);
+// // alert("division is",a/b);
+// alert(Math.max(Math.round(a),Math.round(b)));
+// alert(Math.min(Math.round(a),Math.round(b)));
+// alert(Math.round(Math.random()*10000));
+// var str = "This is my course";
+// console.log(str.split(" ")[0].split('').reverse().join('')+" "+
+// str.split(" ")[1].split('').reverse().join('')+" "+
+// str.split(" ")[2].split('').reverse().join('')+" "+
+// str.split(" ")[3].split('').reverse().join(''));
+
+// function sum(a,b){
+//     return(a+b);
+// }
+// function sub(a,b){
+//     return(a-b);
+// }
+// function mul(a,b){
+//     return(a*b);
+// }
+// function div(a,b){
+//     return(a/b);
+// }
+// var sum=sum(1,2);
+// console.log("sum is",sum)
+// var sum=sub(1,2);
+// console.log("sub is",sub)
+// var sum=mul(1,2);
+// console.log("mul is",mul)
+// var sum=div(1,2);
+// console.log("div is",div)
+
+// function findCharAt(str,index){
+//     return str.charAt(index);
+// }
+// console.log(findCharAt("india",2));
+
+// function area(radius){
+//     return Math.PI*(radius**2);
+// }
+// console.log(area(2))
+
+// function sq(radius){
+//     return radius**2;
+// }
+// console.log(area(2))
+
+// function area(radius){
+//     return Math.PI*(radius**2);
+// }
+// function x(radius,fun){
+//     return fun(radius);
+    
+// }
+
+// console.log(x(2,area))
+
+var arr = [2,4,6,8,10];
+arr.push(12,14)
+console.log(arr);
+arr.pop()
+console.log(arr);
+arr.shift()
+console.log(arr);
+arr.unshift(0)
+console.log(arr);
+arr.splice(2,1,14,16);
+console.log();
+
+
